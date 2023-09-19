f4eafe54a8815cbaaa58cbe5da895211c0b7ebe8
Konstantin <novash.ki@gmail.com>
2023-09-19 10:57:47 +0300

    save

---
+++ run_args_localhost.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd target
+#java -jar squeeze-alice*.jar -lmsip localhost -lmsport 9000 -port 8005 -context cmd
+java -jar squeeze-alice*.jar -lmsip localhost -lmsport 9000 -port 8010 -context cmd
+#$SHELL
