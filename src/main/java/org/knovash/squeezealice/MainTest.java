1aeb06191d70b0bebe3a7ff7da6edc64ddd45f1e
Konstantin <novash.ki@gmail.com>
2024-04-07 02:57:12 +0300

    add test Polymorphic Deserialization

--- src/main/java/org/knovash/squeezealice/MainTest.java
+++ src/main/java/org/knovash/squeezealice/MainTest.java
@@ -1,33 +1,81 @@
 package org.knovash.squeezealice;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.log4j.Log4j2;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import org.knovash.squeezealice.provider.response.CapabilitiesZ;
 
 @Log4j2
 public class MainTest {
 
-
-    private static Object TimeUnit;
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
 
     public static void main(String[] args) {
         log.info("  ---+++===[ START ]===+++---");
-
-  timer();
-
+        try {
+            whenDeserializingPolymorphic_thenCorrect();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void timer(){
-        Runnable drawRunnable = new Runnable() {
-            public void run() {
-                System.out.println("-----");
-            }
-        };
-        ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
-        exec.scheduleAtFixedRate(drawRunnable, 0, 5, java.util.concurrent.TimeUnit.SECONDS);
+    public static void whenDeserializingPolymorphic_thenCorrect() throws JsonProcessingException {
+
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+
+        String jsonF = "{\n" +
+                "  \"type\" : \"devices.capabilities.on_off\",\n" +
+                "  \"retrievable\" : true," +
+                "  \"stateX\" :" +
+                "{" +
+                "  \"instance\" : \"AAA\"," +
+                "  \"relative\" : true" +
+
+                "}"+
+
+                "}";
+        String jsonE = "{\n" +
+                "  \"type\" : \"devices.capabilities.range\",\n" +
+                "  \"retrievable\" : true" +
+
+                "}";
+
+//        JSON to POJO
+
+        CapabilitiesZ capabilitiesE = new ObjectMapper().readerFor(CapabilitiesZ.class).readValue(jsonE);
+        log.info("\n\n" + capabilitiesE);
+        log.info(CapabilitiesZ.CapabilitiesString.class);
+        log.info(capabilitiesE.getClass());
+
+        CapabilitiesZ capabilitiesF = new ObjectMapper().readerFor(CapabilitiesZ.class).readValue(jsonF);
+        log.info("\n\n" + capabilitiesF);
+        log.info(CapabilitiesZ.CapabilitiesBoolean.class);
+        log.info(capabilitiesF.getClass());
+
+//        POJO to JSON
+
+        String jsonNewF;
+        try {
+            jsonNewF = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(capabilitiesF);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("\n\n" + jsonNewF);
+
+        String jsonNewE;
+        try {
+            jsonNewE = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(capabilitiesE);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("\n\n" + jsonNewE);
+
+
     }
-
-
 }
